97fc0d4ba2844b01a4a8fe01f24818a62039155d
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-08-13 17:43:59 +0300

    adding code snippets with bright

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,8 +3,11 @@ import { loadBlogPost } from "@/helpers/file-helpers";
 import BlogHero from "@/components/BlogHero";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { BLOG_TITLE } from "@/constants";
-
+import CodeSnippet from "@/components/CodeSnippet";
 import styles from "./postSlug.module.css";
+import DivisionGroupsDemo from "@/components/DivisionGroupsDemo";
+import CircularColorsDemo from "@/components/CircularColorsDemo";
+
 export async function generateMetadata({ params }) {
   const { postSlug } = params;
   const { frontmatter } = await loadBlogPost(postSlug);
@@ -25,7 +28,14 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote
+          source={content}
+          components={{
+            pre: CodeSnippet,
+            CircularColorsDemo,
+            DivisionGroupsDemo,
+          }}
+        />
       </div>
     </article>
   );
